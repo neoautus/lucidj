6b6fe244f72b3c28d7f7a6be4eb729b57f7cbb8f
Marcond Marchi <marcond@gmail.com>
2017-10-21 23:22:12 -0200

    Added new component ContainerTreeRenderer.

---
+++ modules/ContainerTreeRenderer/src/org/lucidj/containertree/ContainerTreeRenderer.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2017 NEOautus Ltd. (http://neoautus.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.lucidj.containertree;
+
+import org.lucidj.api.Renderer;
+import org.lucidj.api.ServiceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.data.Container;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.server.Resource;
+import com.vaadin.server.Sizeable;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Tree;
+
+import org.osgi.framework.BundleContext;
+
+public class ContainerTreeRenderer extends Tree implements Renderer, ItemClickEvent.ItemClickListener
+{
+    private final static Logger log = LoggerFactory.getLogger (ContainerTreeRenderer.class);
+
+    private Container source;
+
+    public ContainerTreeRenderer (ServiceContext serviceContext, BundleContext bundleContext)
+    {
+        setWidth (100, Sizeable.Unit.PERCENTAGE); // <--- remove?
+        setHeightUndefined ();
+        addStyleName ("x-container-tree-renderer");
+        setSelectable (false);
+        setImmediate (true);
+        addItemClickListener (this);
+    }
+
+    private void update_components ()
+    {
+        // Called on objectLinked() and objectUpdated()
+    }
+
+    public static boolean isCompatible (Object object)
+    {
+        return (object instanceof Container);
+    }
+
+    @Override // Renderer
+    public void objectLinked (Object obj)
+    {
+        source = (Container)obj;
+        setContainerDataSource (source);
+
+        // Try the usual suspects
+        if (source.getType ("Name").isAssignableFrom (String.class))
+        {
+            setItemCaptionPropertyId ("Name");
+        }
+        if (source.getType ("Icon").isAssignableFrom (Resource.class))
+        {
+            setItemIconPropertyId ("Icon");
+        }
+
+        // TODO: FIND A BETTER WAY TO SPECIFY PROPERTIES, USE THIS AS FALLBACK
+        for (Object pid: source.getContainerPropertyIds ())
+        {
+            if (source.getType (pid).isAssignableFrom (String.class)
+                && getItemCaptionPropertyId () == null)
+            {
+                setItemCaptionPropertyId (pid);
+            }
+            else if (source.getType (pid).isAssignableFrom (Resource.class)
+                && getItemIconPropertyId () == null)
+            {
+                setItemIconPropertyId (pid);
+            }
+        }
+        update_components ();
+    }
+
+    @Override // Renderer
+    public void objectUnlinked ()
+    {
+        source = null;
+    }
+
+    @Override // Renderer
+    public Component renderingComponent ()
+    {
+        return (this);
+    }
+
+    @Override // Renderer
+    public void objectUpdated ()
+    {
+        update_components ();
+    }
+
+    @Override // ItemClickEvent.ItemClickListener
+    public void itemClick (ItemClickEvent itemClickEvent)
+    {
+        // There's no need to bubble-up the event since the foreign
+        // listeners are already attached directly to this component
+        // via bypass. We only need to do local housekeeping.
+
+
+        if (itemClickEvent.isDoubleClick ()
+            && itemClickEvent.getSource () instanceof Tree)
+        {
+            // Handles directory expand/contract automatically
+            Tree tree = (Tree)itemClickEvent.getSource ();
+            Object item_id = itemClickEvent.getItemId ();
+
+            if (tree.isExpanded (item_id))
+            {
+                tree.collapseItem (item_id);
+            }
+            else
+            {
+                tree.expandItem (item_id);
+            }
+        }
+    }
+}
+
+// EOF
---
+++ modules/ContainerTreeRenderer/src/org/lucidj/containertree/ContainerTreeRendererProvider.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017 NEOautus Ltd. (http://neoautus.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.lucidj.containertree;
+
+import org.lucidj.api.Renderer;
+import org.lucidj.api.RendererProvider;
+import org.lucidj.api.ServiceContext;
+
+import org.osgi.framework.BundleContext;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Context;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
+
+@Component (immediate = true, publicFactory = false)
+@Instantiate
+@Provides
+public class ContainerTreeRendererProvider implements RendererProvider
+{
+    @Requires
+    private ServiceContext serviceContext;
+
+    @Context
+    private BundleContext bundleContext;
+
+    @Override
+    public Renderer getCompatibleRenderer (Object object)
+    {
+        if (ContainerTreeRenderer.isCompatible (object))
+        {
+            return (serviceContext.newServiceObject (ContainerTreeRenderer.class));
+        }
+        return (null);
+    }
+
+    @Validate
+    private void validate ()
+    {
+        serviceContext.publishUrl (bundleContext, "/public/styles.css");
+        serviceContext.register (ContainerTreeRenderer.class);
+    }
+}
+
+// EOF
